c74c994bc5fb5abe9211dd5a97ef4e296dadb056
LaetitiaZgraja <laetitia.zgraja@seenovate.com>
2025-12-02 15:28:20 +0100

    Update streamlit_app.py
    mise en forme json par dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,5 @@ if ingredients_list:
             st.success('Your Smoothie is ordered! '+name_order, icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-
+#st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothieroot_response.json(),use_container_width=True)
